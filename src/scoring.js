f235d1d2df5ae55c50735802a46f8f59c8e8b421
b-rucel <me@brucelim.com>
2016-01-04 17:37:53 -0800

    adding scoring methods to sdk. will need to discuss which events to open.
    also added markdown documentation file for scoring section.

---
+++ src/scoring.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var EventEmitter = require('events');
+var extend = require('extend');
+var superagent = require('superagent');
+
+var Scoring = module.exports = Object.assign( {}, EventEmitter.prototype );
+
+var DEFAULTS = {
+    host: 'api-auth.hellofloat.com'
+};
+
+Scoring.init = function( options ) {
+    var self = this;
+    self.options = extend( true, {}, DEFAULTS, options );
+    return self;
+};
+
+
+Scoring.addBank = function(overlay, cb) {
+    var self = this;
+
+    superagent
+        .post('https://' + self.options.host + '/source/bankaccount')
+        .send(overlay)
+        .end(function(error, response) {
+            if(error) {
+                cb(response && response.body ? response.body : error);
+                return;
+            }
+
+            callback(null, response);
+            console.log('\u000A');
+            console.log('---- response.body ----');
+            console.log(response.body);
+        });
+
+};
+
+
+Scoring.getBankAccount = function(cb) {
+    var self = this;
+
+    superagent
+        .get('https://' + self.options.host + '/accounts')
+        .end(function(error, response) {
+            if(error) {
+                cb(response && response.body ? response.body : error);
+                return;
+            }
+
+            callback(null, response);
+        });
+
+};
+
+
+
+Scoring.getScore = function(cb) {
+    var self = this;
+
+    superagent
+        .get('https://' + self.options.host + '/accounts')
+        .end(function(error, response) {
+            if(error) {
+                cb(response && response.body ? response.body : error);
+                return;
+            }
+
+            callback(null, response);
+        });
+
+};
